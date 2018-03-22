888a5d2874408ae1dae4794a5fe9049e9a763780
Carlo Jeske <jeske@sc-networks.com>
2018-03-22 11:18:49 +0100

    [dev] added development config

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 module.exports = {
-  develop: require('../configs/webpack.base.conf.js'),
+  dev: require('../configs/webpack.dev.conf.js'),
   prod: require('../configs/webpack.prod.conf.js'),
   test: require('../configs/webpack.test.conf.js'),
 };
