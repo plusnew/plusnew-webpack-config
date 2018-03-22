130f99259c982c555c045b99b8c1f528017de2ed
Carlo Jeske <jeske@sc-networks.com>
2018-03-22 11:08:33 +0100

    [configs] initial commit

---
+++ configs/webpack.prod.conf.js
@@ -0,0 +1,5 @@
+const config = require('./webpack.base.conf.js');
+
+config.mode = 'production';
+
+module.exports = config;
---
+++ src/index.js
@@ -0,0 +1,5 @@
+module.exports = {
+  develop: require('../configs/webpack.base.conf.js'),
+  prod: require('../configs/webpack.prod.conf.js'),
+  test: require('../configs/webpack.test.conf.js'),
+};
